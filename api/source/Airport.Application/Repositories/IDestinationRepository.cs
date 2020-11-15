653a55da690328f41f5e4d65a126f6c56390a266
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-11-15 17:15:45 +0200

    Create Destination Repository

---
+++ api/source/Airport.Application/Repositories/IDestinationRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Airport.Domain.Destination;
+
+namespace Airport.Application.Repositories
+{
+    public interface IDestinationRepository
+    {
+        Task Add(Destination destination);
+    }
+}
---
+++ api/source/Airport.Infrastructure/DapperDataAccess/Repositories/DestinationRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+using Airport.Application.Repositories;
+using Airport.Domain.Destination;
+using Dapper;
+
+namespace Airport.Infrastructure.DapperDataAccess.Repositories
+{
+    public class DestinationRepository : IDestinationRepository
+    {
+        private readonly IDbConnection _dbConnection;
+
+        public DestinationRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+
+        public async Task Add(Destination destination)
+        {
+            const string insertDestinationQuery =
+                "INSERT INTO destinations (airlineId, country) SELECT @AirlineId, @Country WHERE NOT EXISTS (SELECT airlineId, country FROM destinations WHERE airlineId=@AirlineId AND country=@country)";
+            await _dbConnection.ExecuteAsync(insertDestinationQuery, destination);
+        }
+    }
+}
