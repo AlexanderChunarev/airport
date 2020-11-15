e36d37bd321bdae64dfd15f42d4e1cfa48ab9113
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-11-15 17:16:22 +0200

    Create Destination Migration

---
+++ api/source/Airport.Infrastructure/DapperDataAccess/Migrations/202015111633_DestinationMigration.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace Airport.Infrastructure.DapperDataAccess.Migrations
+{
+    [Migration(202015111633, TransactionBehavior.None)]
+    public class DestinationMigration : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("destinations")
+                .WithColumn("id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("airlineid").AsInt32()
+                .WithColumn("country").AsString();
+
+            Create.ForeignKey() 
+                .FromTable("destinations").ForeignColumn("airlineid")
+                .ToTable("airlines").PrimaryColumn("id");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("destinations");
+        }
+    }
+}
