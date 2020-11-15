21f1d528ea3fffa254cfcb8fc6bf951c2799d417
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-11-15 15:53:58 +0200

    Create Plane model

---
+++ api/source/Airport.Domain/Plane/Plane.cs
@@ -0,0 +1,10 @@
+﻿namespace Airport.Domain.Plane
+{
+    public class Plane
+    {
+        public int Id { get; set; }
+        public int AirlineId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
