bd510528b808f98d5425c21a0c5943cb386e8cb7
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-11-15 17:14:13 +0200

    Create Destination model

---
+++ api/source/Airport.Domain/Destination/Destination.cs
@@ -0,0 +1,9 @@
+﻿namespace Airport.Domain.Destination
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public int AirlineId { get; set; }
+        public string Country { get; set; }
+    }
+}
