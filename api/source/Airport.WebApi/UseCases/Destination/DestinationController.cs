6ecb6922e4590539ca63db7117b22c4ae2ffe38e
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-11-15 17:17:24 +0200

    Create Destination WebApi services

---
+++ api/source/Airport.WebApi/UseCases/Destination/DestinationController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Airport.Application.Boundaries.Destination;
+using Airport.Application.UseCases.Destination;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Airport.WebApi.UseCases.Destination
+{
+    using Airport.Domain.Destination;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DestinationController : Controller
+    {
+        private readonly IAddDestinationUseCase _addDestinationUseCase;
+        private readonly DestinationPresenter _destinationPresenter;
+
+        public DestinationController(IAddDestinationUseCase addDestinationUseCase,
+            DestinationPresenter destinationPresenter)
+        {
+            _addDestinationUseCase = addDestinationUseCase;
+            _destinationPresenter = destinationPresenter;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddDestination([FromBody] Destination destination)
+        {
+            var destinationInput = new DestinationInput(destination);
+            await _addDestinationUseCase.Execute(destinationInput);
+            return _destinationPresenter.ViewModel;
+        }
+    }
+}
