b7f27db3357c438ca5007916ec0ce2eec94833ac
AlexanderChunarev <alexanderchunarov1999@gmail.com>
2020-11-15 17:14:54 +0200

    Create Destination boundaries

---
+++ api/source/Airport.Application/Boundaries/Destination/DestinationInput.cs
@@ -0,0 +1,14 @@
+﻿namespace Airport.Application.Boundaries.Destination
+{
+    using Airport.Domain.Destination;
+
+    public class DestinationInput
+    {
+        public Destination Destination { get; set; }
+
+        public DestinationInput(Destination destination)
+        {
+            Destination = destination;
+        }
+    }
+}
---
+++ api/source/Airport.Application/Boundaries/Destination/DestinationOutput.cs
@@ -0,0 +1,14 @@
+﻿namespace Airport.Application.Boundaries.Destination
+{
+    using Airport.Domain.Destination;
+
+    public sealed class DestinationOutput
+    {
+        public Destination Destination { get; set; }
+
+        public DestinationOutput(Destination destination)
+        {
+            Destination = destination;
+        }
+    }
+}
